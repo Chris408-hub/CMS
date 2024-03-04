19cd2d83719b19e6181e544fe09098ca24d0725d
Chris408-hub <112998909+Chris408-hub@users.noreply.github.com>
2024-03-04 01:22:13 -0500

    change code

--- admin/add_project.php
+++ admin/add_project.php
@@ -4,25 +4,25 @@ require_once('../includes/connect.php');
 
 if (isset($_POST['submit'])) {
 
-    // 处理项目图片上传
+    // upload images
 
     $image_target_file = '../images/img_'.time();
-
+    $target_dir = "../images/"; 
     $imageFileType = strtolower(pathinfo($_FILES['project_image']['name'], PATHINFO_EXTENSION));
-    $target_file .= '.'.$imageFileType;
-      move_uploaded_file($_FILES['project_image']['tmp_name'], $image_target_file);
-$imageName = 'img_' . time();
+    $filename = uniqid() . "." . $imageFileType; 
+    $target_file = $target_dir . $filename; 
+    move_uploaded_file($_FILES['project_image']['tmp_name'], $target_file);
  
-    // 处理项目文件夹上传
+    // upload project
 
-    if (isset($_FILES['project_folder']) && $_FILES['project_folder']['error'] == 0) {
-        // 移动上传的ZIP文件到目标位置
+    if (isset($_FILES['project_url']) && $_FILES['project_url']['error'] == 0) {
+        // move zip file
         $folderName = 'project_' . time();
         $folder_target_file = '../case-study/' . $folderName . '.zip';
 
         move_uploaded_file($_FILES['project_url']['tmp_name'], $folder_target_file);
 
-        // 解压ZIP文件
+        // unZIP
         $zip = new ZipArchive;
         if ($zip->open($folder_target_file) === TRUE) {
             $extractPath = '../case-study/' . $folderName; // 设置解压路径
@@ -32,29 +32,28 @@ $imageName = 'img_' . time();
             // 设置项目URL指向解压后的index.html文件
             
             $projectFolder = 'case-study/' . $folderName;
-            // $projectURL = $projectFolder .'/index.html';
-       
+            $projectURL = $projectFolder .'/index.html';
+        }
+    }
 
-
-    // 插入数据库
+    // insert db
     $query = "INSERT INTO tbl_project (project_title, project_url, project_image, project_type) VALUES (?, ?, ?, ?)";
     $stmt = $connection->prepare($query);
     $stmt->bindParam(1,$_POST['project_title'],PDO::PARAM_STR);
     $stmt->bindParam(2, $projectFolder,PDO::PARAM_STR);
-    $stmt->bindParam(3,$imageName,PDO::PARAM_STR);
+    $stmt->bindParam(3,$filename,PDO::PARAM_STR);
     $stmt->bindParam(4,$_POST['project_type'],PDO::PARAM_STR);
 
     $stmt->execute();
     $last_id = $connection->lastInsertId();
     $stmt = null;
     header('Location: project_list.php');
+    exit();
 
 
 
-
-        }
-    }
 }
+
 ?>
 
 
